c278b7540dad9dde41ef4766a1afff7457f05a82
ProgrammerGreenhorn <1961005093@qq.com>
2025-01-04 15:17:16 +0800

    A little modify

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -9,6 +9,7 @@
 #include "riscv.h"
 #include "defs.h"
 
+
 void freerange(void *pa_start, void *pa_end);
 
 extern char end[]; // first address after kernel.
@@ -22,29 +23,25 @@ struct cpu_freelist {
   struct spinlock lock;
   struct run *freelist;
   char lock_name[8];
-} kmems[NCPU];// lab8-1, per cpu a freelist
+} kmems[NCPU]; // lab8-1, per cpu a freelist
 
-
-void
-kinit()
-{ int i;
+void kinit() {
+  int i;
   // lab 8-1
-  for(i = 0;i < NCPU;++i){
-    snprintf(kmems[i].lock_name, 8,"kmem_d%d", i);
-    initlock(&kmems[i].lock,kmems[i].lock_name);
+  for (i = 0; i < NCPU; ++i) {
+    snprintf(kmems[i].lock_name, 8, "kmem_d%d", i);
+    initlock(&kmems[i].lock, kmems[i].lock_name);
   }
   // because the kinit is called by the cpu 0,
   // so we give all memory to it, and when other
   // cpu need memory, they came to steal from cpu 0's freelist
-  freerange(end, (void*)PHYSTOP);
+  freerange(end, (void *)PHYSTOP);
 }
 
-void
-freerange(void *pa_start, void *pa_end)
-{
+void freerange(void *pa_start, void *pa_end) {
   char *p;
-  p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  p = (char *)PGROUNDUP((uint64)pa_start);
+  for (; p + PGSIZE <= (char *)pa_end; p += PGSIZE)
     kfree(p);
 }
 
@@ -52,19 +49,17 @@ freerange(void *pa_start, void *pa_end)
 // which normally should have been returned by a
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
-void
-kfree(void *pa)
-{
+void kfree(void *pa) {
   struct run *r;
   int id;
-  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+  if (((uint64)pa % PGSIZE) != 0 || (char *)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
-  r = (struct run*)pa;
-  // lab8-1 turn off interrupt,give the free mem to the current cpu's freelist 
+  r = (struct run *)pa;
+  // lab8-1 turn off interrupt,give the free mem to the current cpu's freelist
   push_off();
   id = cpuid();
   pop_off();
@@ -74,30 +69,29 @@ kfree(void *pa)
   release(&kmems[id].lock);
 }
 
-struct run *steal(int id){
-  int i;
-  int c = id;
-  struct run *fast,*slow,*head;
-  if(cpuid() != id)
+struct run *steal(int id) {
+  struct run *fast, *slow, *head;
+  if (cpuid() != id)
     panic("steal");
-  for(i = 1; i < NCPU; ++i){
-    // c is the current cpu wo try to steal
-    c = (c + 1) % NCPU;
-    acquire(&kmems[c].lock);
-    if(kmems[c].freelist){
-      // steal the before half from it
-      slow = head = kmems[c].freelist;
-      fast = slow->next;
-      while(fast && fast->next){
+
+  for (int i = 0; i < NCPU; ++i) {
+    if (i == id)
+      continue;
+    acquire(&kmems[i].lock);
+    if (kmems[i].freelist) {
+      head = slow = fast = kmems[i].freelist;
+      while (fast && fast->next) {
         fast = fast->next->next;
         slow = slow->next;
       }
-      kmems[c].freelist = slow->next;
-      release(&kmems[c].lock);
+      // update the one we steal's head to slow->next
+      // we steal the befor half from it
+      kmems[i].freelist = slow->next;
       slow->next = 0;
+      release(&kmems[i].lock);
       return head;
     }
-    release(&kmems[c].lock);
+    release(&kmems[i].lock);
   }
   return 0;
 }
@@ -105,9 +99,7 @@ struct run *steal(int id){
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
 // Returns 0 if the memory cannot be allocated.
-void *
-kalloc(void)
-{
+void *kalloc(void) {
   struct run *r;
   int id;
   push_off();
@@ -115,19 +107,20 @@ kalloc(void)
   pop_off();
   acquire(&kmems[id].lock);
   r = kmems[id].freelist;
-  if(r)
+  if (r)
     kmems[id].freelist = r->next;
   release(&kmems[id].lock);
   // lab 8-1 steal page from other cpu
-  if(!r && (r = steal(id))){
-    acquire(&kmems[id].lock);
-    kmems[id].freelist = r->next;
-    release(&kmems[id].lock);
+  if (!r) {
+    if ((r = steal(id))) {
+      acquire(&kmems[id].lock);
+      kmems[id].freelist = r->next;
+      release(&kmems[id].lock);
+    }
   }
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
+  if (r)
+    memset((char *)r, 5, PGSIZE); // fill with junk
 
-  return (void*)r;
+  return (void *)r;
 }
-
